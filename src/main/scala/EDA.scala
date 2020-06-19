d1d3f6bba9080102355c1f897753139101fc2a8b
TrinhDinhPhuc <phuccoi996@gmail.com>
2020-06-19 17:07:04 +0900

    auto updates

--- src/main/scala/EDA.scala
+++ src/main/scala/EDA.scala
@@ -2,7 +2,9 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 object EDA {
   def reviewsDetailDF_EDA(sparkSession: SparkSession,reviewsDetailDF:DataFrame): Unit ={
-    println(reviewsDetailDF.filter(reviewsDetailDF("reviewer_id").isNull || reviewsDetailDF("reviewer_id") === "" || reviewsDetailDF("reviewer_id").isNaN).count())
-
+    println(reviewsDetailDF.filter(reviewsDetailDF("reviewer_id").isNull ).count())
+    println(reviewsDetailDF.filter(reviewsDetailDF("reviewer_id").isNaN).count())
+    println(reviewsDetailDF.filter(reviewsDetailDF("reviewer_id") === "").count())
+    println(reviewsDetailDF.count())
   }
 }
